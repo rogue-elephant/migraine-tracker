d807f2cb322bb0dc490dcacd68c87f372369e990
DRandell <jup.trandell@gmail.com>
2019-10-30 12:26:23 +0000

    Initial poc of migraine list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,7 +17,7 @@ export class AppComponent {
       icon: 'home'
     },
     {
-      title: 'List',
+      title: 'History',
       url: '/list',
       icon: 'list'
     }
--- src/app/list/list.page.ts
+++ src/app/list/list.page.ts
@@ -7,25 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListPage implements OnInit {
   private selectedItem: any;
-  private icons = [
-    'flask',
-    'wifi',
-    'beer',
-    'football',
-    'basketball',
-    'paper-plane',
-    'american-football',
-    'boat',
-    'bluetooth',
-    'build'
-  ];
-  public items: Array<{ title: string; note: string; icon: string }> = [];
+  public items: Array<{ severity: number, title: string; note: string; icon: string }> = [];
   constructor() {
     for (let i = 1; i < 11; i++) {
+      const severity = Math.floor(Math.random() * 10);
       this.items.push({
-        title: 'Item ' + i,
+        severity: severity,
+        title: `Migraine: ${severity} out of 10`,
         note: 'This is item #' + i,
-        icon: this.icons[Math.floor(Math.random() * this.icons.length)]
+        icon: 'flash'
       });
     }
   }
